e396c06ba18c1acd5da156e5eb89566b75dc6ead
devsocket <venkatasudheerkumar.kondeti@gmail.com>
2026-03-12 23:53:33 -0300

    Add Terraform configuration for spoke connectivity, including backend, variables, and outputs

---
+++ connectivity/spokes/dev/main.tf
@@ -0,0 +1,51 @@
+# Remote state - Hub
+# Reads output from connectivity/hub/outputs.tf
+# This is how spoke knows hub Vnet ID, name and rg
+# Without hardcoding these values here
+
+data "terraform_remote_state" "hub" {
+    backend = "azurerm"
+
+    config = {
+        resource_group_name = var.tfstate_resource_group_name
+        storage_account_name = var.tfstate_storage_account_name
+        container_name = var.tfstate_container_name
+        key = "connectivity/hub.tfstate"
+    }
+}
+
+# Spoke Vnet
+module "spoke_vnet" {
+    source = "git::https://github.com/devsocket/terraform-common-modules.git/modules/connectivity/spoke_vnet"
+
+    #Spoke Identity
+    resource_group_name = var.resource_group_name
+    location = var.location
+    vnet_name = var.vnet_name
+    vnet_address_space = var.vnet_address_space
+
+    #Subnets
+    aks_subnet_name = var.aks_subnet_name
+    aks_subnet_cidr = var.aks_subnet_cidr
+    appgw_subnet_name = var.appgw_subnet_name
+    app_subnet_cidr = var.appgw_subnet_cidr
+    private_endpoints_subnet_name = var.private_endpoints_subnet_name
+    private_endpoints_subnet_cidr = var.private_endpoints_subnet_cidr
+
+    #Hub values - read from remote state, not hardcoded
+    hub_vnet_id = data.terraform_remote_state.hub.outputs.vnet_id
+    hub_vnet_name = data.terraform_remote_state.hub.outputs.vnet_name
+    hub_resource_group_name = data.terraform_remote_state.hub.outputs.resource_group_name
+
+    # Route Table
+    route_table_name = var.route_table_name
+    disable_bgp_route_propogation = var.disable_bgp_route_propogation
+
+    tags = var.tags
+
+    # Tells the module provider to use the connectivity alias when creating spoke vnet resources, so that it goes through the hub subscription and not the default subscription of the landing zone
+    providers = {
+        azurerm = azurerm
+        azurerm.connectivity = azurerm.connectivity
+    }
+}
---
+++ connectivity/spokes/dev/variables.tf
@@ -0,0 +1,107 @@
+variable "connectivity_subscription_id" {
+    description = "Subscription ID of the connectivity subscription"
+    type = string
+    #Never hardcode Subscription IDs
+}
+
+variable "location" {
+    description = "Azure region for spoke resources"
+    type = string
+    default = "canadacentral"
+}
+
+variable "resource_group_name" {
+    description = "resource group name for spoke Vnet and resources"
+    type = string
+    default = "rg-devsocket-cc-spoke-dev"
+}
+
+variable "vnet_name" {
+    description = "Dev vnet name"
+    type = string
+    default = "vnet-devsocket-spoke-dev"
+}
+
+variable "vnet_address_space" {
+    description "dev spoke vnet address space"
+    type = list(string)
+    default = ["10.1.0.0/16"]
+}
+
+# Subnets 
+variable "aks_subnet_name" {
+    description = "Aks node pool subnet name"
+    type = string
+    default = "snet-aks"
+}
+
+variable "aks_subnet_cidr" {
+    description = "CIDR for aks subnet pool"
+    type = string
+    default = "10.1.0.0/22"
+}
+
+variable "appgw_subnet_name" {
+    description = "Application Gateway subnet name"
+    type  = String
+    default = "snet-appgw"
+}
+variable "appgw_subnet_cidr" {
+    description = "CIDR for Application Gateway subnet"
+    type = string
+    default = "10.1.4.0/24"
+}
+variable "private_endpoints_subnet_name" {
+    description = "Subnet name of private endpoints"
+    type = string
+    default = "snet-privateendpoints"
+}
+variable "private_endpoints_subnet_cidr" {
+    description = "CIDR of private endpoints"
+    type = string
+    default = "10.1.5.0/27"
+}
+# Route Table
+variable "route_table_name" {
+    description = "Name of the routing table attached to the spoke vnet"
+    type = string
+    default = "rt-devsocket-spoke-dev"
+}
+
+variable "disable_bgp_route_propogation" {
+    description = "Disable BGP route propogation. Set true when routing through firewall"
+    type = bool
+    default = false
+}
+
+# Remote State
+variable "tfstate_resource_group_name" {
+    description = "Resource group name of the remote state storage account"
+    type = string
+    # fill from boot strapping output
+}
+
+variable "tfstate_storage_account_name" {
+    decription = "Storage account name of remote state storage account generated during boottrap phasse"
+    type = string
+    # fill from bootstrap output
+}
+
+variable "tfstate_storage_container" {
+    description = "Storage container name of the remote state storage account"
+    type = string
+    #fill from bootstrap output
+}
+
+# Tags
+variable "tags" {
+    description = "Tags applied to all resources in this deployment"
+    type = map(string)
+    default = {
+        environment = "dev"
+        managed_by = "terraform"
+        project = "devsocket-landing-zone"
+        layer = "spoke-network"
+    }
+}
+
