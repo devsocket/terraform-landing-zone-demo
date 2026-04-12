0bef37bceb4591de3f2c57eb98a4ba185a9a20ae
devsocket <venkatasudheerkumar.kondeti@gmail.com>
2026-04-12 16:08:03 -0300

    chore: update module sources in Terraform configurations to remove version references

--- shared/storage/main.tf
+++ shared/storage/main.tf
@@ -13,7 +13,7 @@ data "terraform_remote_state" "log_analytics" {
 }
 
 module "storage_account" {
-  source = "github.com/devsocket/terraform-common-modules//modules/app_platform/storage?ref=v1.0.0"
+  source = "github.com/devsocket/terraform-common-modules//modules/app_platform/storage"
 
   resource_group_name       = var.resource_group_name
   location                  = var.location
